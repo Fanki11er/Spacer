ef5e3f43630c0917823e552576c4e53f82ecf251
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2019-05-11 21:15:34 +0200

    Repair my mistake

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="app">
-    <div class="nav">
-      <router-link to="./home">Home</router-link>
-      <router-link to="./about">About</router-link>
-    </div>
-    <router-view />
     
+    <router-view />
   </div>
 </template>
 
---
+++ src/components/Claim.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="wrapper">
+        <h1 class="claim">SPACER</h1>
+        <p class="subclaim">
+            Begin your jurney trough our amazing galaxy,
+            and discover places you never even heared of.  
+        </p>
+        <p class="subclaim">
+            Type anything space-related to start
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Claim',
+}
+</script>
+
+<style lang='scss' scoped>
+.wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+}
+
+</style>
